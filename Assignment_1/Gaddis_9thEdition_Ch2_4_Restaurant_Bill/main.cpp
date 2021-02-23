067ead0554c79d78037b19451846a2c28df04257
danmaps <dannybmcvey@gmail.com>
2021-02-23 13:28:33 -0800

    continuing hw1

---
+++ Assignment_1/Gaddis_9thEdition_Ch2_4_Restaurant_Bill/main.cpp
@@ -0,0 +1,56 @@
+/*
+    Author:  Danny McVey
+    Date:    Feb 23, 2021  1:08pm
+    Purpose: Computes the tax and tip on a restaurant bill for a patron with a
+             $88.67 meal charge. The tax should be 6.75 percent of the meal
+ *           cost. The tip should be 20 percent of the total after adding the tax. 
+    Version: 1
+ */
+
+//System Libraries
+#include <iostream>    //Input/Output Library
+using namespace std;   //Library Name-space
+
+//User Libraries
+
+//Global/Universal Constants -- No Global Variables
+//Science, Math, Conversions, Higher Dimensioned constants only
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {  
+    //Declare variables
+    float meal,taxRate,tip; //inputs
+    float taxAmt,tipAmt,subTotal,total;
+    
+    //Initialize variables
+    meal = 88.67;
+    taxRate = 0.0675;
+    tip = 0.20;
+    
+    //Process, map inputs to outputs
+    taxAmt = meal*taxRate;
+    subTotal = meal + taxAmt;
+    tipAmt = subTotal*tip;
+    total = subTotal + tipAmt;
+    
+    //Display initial conditions as well as outputs.
+    std::cout.precision(2);
+    
+    cout<<"meal cost: ";
+    cout<<"$"<<std::fixed<<meal<<endl;
+    
+    cout<<"tax amount: ";
+    cout<<"$"<<taxAmt<<endl;
+    
+    cout<<"tip amount: ";
+    cout<<"$"<<tipAmt<<endl;
+    
+    cout<<"total bill: ";
+    cout<<"$"<<total<<endl;
+    
+    //Exit stage right
+    return 0;
+}
+
