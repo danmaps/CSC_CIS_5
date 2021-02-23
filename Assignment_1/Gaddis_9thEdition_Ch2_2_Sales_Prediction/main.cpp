e6bae41ef244256cedae69bba782c217463518e1
danmaps <dannybmcvey@gmail.com>
2021-02-23 12:21:14 -0800

    continuing hw1

---
+++ Assignment_1/Gaddis_9thEdition_Ch2_2_Sales_Prediction/main.cpp
@@ -0,0 +1,43 @@
+/*
+    Author:  Danny McVey
+    Date:    February 23rd, 2021  12:09pm
+    Purpose: Sales Prediction
+        The East Coast sales division of a company generates 58 percent of total
+        sales. Based on that percentage, this program that predicts how much the 
+        East Coast division will generate if the company has $8.6 million in
+        sales this year.
+    Version: 1
+ */
+
+//System Libraries
+#include <iostream>    //Input/Output Library
+using namespace std;   //Library Name-space
+
+//User Libraries
+
+//Global/Universal Constants -- No Global Variables
+//Science, Math, Conversions, Higher Dimensioned constants only
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {  
+    //Declare variables
+    float sales,
+          eCoast;
+    
+    //Initialize variables
+    sales = 8.6e6f;
+    
+    //Process, map inputs to outputs
+    eCoast = 0.58 * sales;
+    
+    //Display initial conditions as well as outputs.
+    cout<<"East Coast division: ";
+    std::cout.precision(2);
+    cout<<"$"<<std::fixed<<eCoast;
+    
+    //Exit stage right
+    return 0;
+}
+
