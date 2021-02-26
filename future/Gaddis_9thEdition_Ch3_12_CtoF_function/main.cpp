c73ded59c3dd63896b91cdae68e475e2d7d2a286
danmaps <dannybmcvey@gmail.com>
2021-02-26 11:01:35 -0800

    chapter 3 problems

---
+++ future/Gaddis_9thEdition_Ch3_12_CtoF_function/main.cpp
@@ -0,0 +1,41 @@
+/*
+    Author:  Danny McVey
+    Date:    February 26, 2021, 10:19 AM
+    Purpose: converts Celsius temperatures to Fahrenheit temperatures
+             uses a function and for loop to show 5 results
+    Version: 1
+ */
+
+//System Libraries
+#include <iostream>    //Input/Output Library
+using namespace std; //Library Name-space
+
+//User Libraries
+
+//Global/Universal Constants -- No Global Variables
+//Science, Math, Conversions, Higher Dimensioned constants only
+
+//Function Prototypes
+
+//Execution Begins Here
+
+float cToF(float c){
+    float f = ((9.0/5.0)*c)+32;
+    return f;
+}
+
+int main(int argc, char** argv) {
+    float cels, fahr;
+    
+    float temps[5]={20,25,30,35,40}; //array of input values
+    
+    for (int i=0;i<5;i++){ // loop through all 5 inputs
+        fahr = cToF(temps[i]); // use the function to do the conversion
+        
+        cout<<temps[i]<<"c = "; // display the input temp
+        cout<<fahr<<"f"<<endl; // display the converted output temp
+    } // end for loop block
+
+    return 0;
+}
+
